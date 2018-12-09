449e7e63d04c4c01ebb7d19eb5f0750849532b64
Edgar Bruney <Brun3y@gmail.com>
2018-12-08 18:23:17 -0600

    Slides

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+$( document ).ready(function() {
+   
+
+});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "500px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
