eaa31f726f4a264993cf6dcae1150d1be787d6a1
Edgar Bruney <Brun3y@gmail.com>
2018-12-26 12:59:30 -0800

    Progreso

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-$( document ).ready(function() {
-   
+$(document).ready(function () {
+
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#cartoonVideo").attr('src');
+
+    /* Remove iframe src attribute on page load to
+    prevent autoplay in background */
+    $("#cartoonVideo").attr('src', '');
+
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed */
+    $("#myModal").on('shown.bs.modal', function () {
+        $("#cartoonVideo").attr('src', url);
+    });
+
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal").on('hide.bs.modal', function () {
+        $("#cartoonVideo").attr('src', '');
+    });
+
+    var retrievedObject = localStorage.getItem('sections');
+
+    if (retrievedObject != null) {
+        $("html, body").animate({
+            scrollTop: $('#' + retrievedObject).offset().top
+        }, 1000);
+        localStorage.clear();
+    }
+
 
 });
 
@@ -11,4 +40,8 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
+}
+
+function setSection(section) {
+    localStorage.setItem('sections', section);
 }
